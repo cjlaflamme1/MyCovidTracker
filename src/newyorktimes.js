bbdee7cea394a02a991f0ca20ac70769a1cdf381
BR3NDAN <br3ndan.l8n@gmail.com>
2020-07-29 21:30:36 -0400

    fixed prior issue and now working through another with getting article titles

--- src/newyorktimes.js
+++ src/newyorktimes.js
@@ -1,33 +1,44 @@
 $('document').ready(function () {
 
-let userInputTerm = 'Coronavirus';
-let userInputSort = 'newest';
+    let userInputTerm = '';
+    let userInputSort = '';
 
-// NY Times API Object
-const apiNYTimes = {
-    searchTerm: userInputTerm,  // default to 'Coronavirus'
-    searchSort: userInputSort,  // default to 'newest'
-    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${this.searchTerm}&sort=${this.searchSort}&api-key=9dBz5iLUOkToYiTEjcz0mgrNxq65pGzm`,
-    checkInputs() {
-        if (userInputTerm === '') {
-            userInputTerm = 'Coronavirus';
+    // NY Times API Object
+    const apiNYTimes = {
+        searchTerm: userInputTerm,  // default to 'Coronavirus'
+        searchSort: userInputSort,  // default to 'newest'
+        checkInputs() {
+            if (userInputTerm === '') {
+                userInputTerm = 'Coronavirus';
+            }
+            if (userInputSort === '') {
+                userInputSort = 'newest';
+            }
+        },
+        call() {
+            this.checkInputs();
+            $.ajax({
+                url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${apiNYTimes.searchTerm}&sort=${apiNYTimes.searchSort}&api-key=9dBz5iLUOkToYiTEjcz0mgrNxq65pGzm`,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response.response.docs);  // r.r.docs accesses 10 first articles to match search criteria
+                
+            });
+            return response;
+        },
+        topArticles() {
+            apiNYTimes.call();
+            const articleDataVars = {
+                headline: response.response.docs[i].headline.main,
+            }
+            for (i = 0; i < 5; i++) {
+                console.log(articleDataVars.headline);
+            }
         }
-        if (userInputSort === '') {
-            userInputSort = 'newest';
-        }
-    },
-    call() {
-        $.ajax({
-            url: this.url,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response.response.docs);  // r.r.docs accesses 10 first articles to match search criteria
-        });
-    },
-    
-}
 
-apiNYTimes.call();
-console.log(apiNYTimes.url);
+    }
+
+    // apiNYTimes.call();
+    apiNYTimes.topArticles();
 
 });
