6c81209bd9e568977d0c70dd3ccc951e755291a1
BR3NDAN <br3ndan.l8n@gmail.com>
2020-07-29 20:57:03 -0400

    mayhave fixed most of the issue with the NYTimes api but now NYTimes is giving me a 400 Bad Request

--- src/newyorktimes.js
+++ src/newyorktimes.js
@@ -1,26 +1,13 @@
 $('document').ready(function () {
 
-let userInputTerm = '';
-let userInputSort = '';
+let userInputTerm = 'Coronavirus';
+let userInputSort = 'newest';
 
 // NY Times API Object
 const apiNYTimes = {
-    apiKey: '9dBz5iLUOkToYiTEjcz0mgrNxq65pGzm',
-    searchTerm: `q=${userInputTerm}`,  // default to 'Coronavirus'
-    searchSort: `&sort=${userInputSort}`,  // default to 'newest'
-    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?
-    ${$(this).searchTerm}
-    ${$(this).searchSort}&api-key=
-    ${$(this).apiKey}`,
-    call() {
-        this.checkInputs();
-        $.ajax({
-            url: this.url,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response.response.docs);  // r.r.docs accesses 10 first articles to match search criteria
-        });
-    },
+    searchTerm: userInputTerm,  // default to 'Coronavirus'
+    searchSort: userInputSort,  // default to 'newest'
+    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${this.searchTerm}&sort=${this.searchSort}&api-key=9dBz5iLUOkToYiTEjcz0mgrNxq65pGzm`,
     checkInputs() {
         if (userInputTerm === '') {
             userInputTerm = 'Coronavirus';
@@ -29,9 +16,18 @@ const apiNYTimes = {
             userInputSort = 'newest';
         }
     },
+    call() {
+        $.ajax({
+            url: this.url,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response.response.docs);  // r.r.docs accesses 10 first articles to match search criteria
+        });
+    },
     
 }
 
-// apiNYTimes.call();
+apiNYTimes.call();
+console.log(apiNYTimes.url);
 
 });
