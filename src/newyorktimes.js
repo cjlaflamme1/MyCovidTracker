5089510669508a6f6b076f2b5d1e0fde2a9acea7
BR3NDAN <br3ndan.l8n@gmail.com>
2020-08-01 12:25:18 -0400

    Displaying Articles to data.html ... dynamically generating the articles HTML elements

--- src/newyorktimes.js
+++ src/newyorktimes.js
@@ -1,44 +1,40 @@
 $('document').ready(function () {
 
-    let userInputTerm = '';
-    let userInputSort = '';
+    const newsArticles = $('#newsArticles');
 
     // NY Times API Object
     const apiNYTimes = {
-        searchTerm: userInputTerm,  // default to 'Coronavirus'
-        searchSort: userInputSort,  // default to 'newest'
-        checkInputs() {
-            if (userInputTerm === '') {
-                userInputTerm = 'Coronavirus';
-            }
-            if (userInputSort === '') {
-                userInputSort = 'newest';
-            }
+        refactorInputLocation() {
+            console.log('placeholder');
         },
         call() {
-            this.checkInputs();
+            const test = '';
+            let userInputLocation = `+${test}`;
             $.ajax({
-                url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${apiNYTimes.searchTerm}&sort=${apiNYTimes.searchSort}&api-key=9dBz5iLUOkToYiTEjcz0mgrNxq65pGzm`,
+                url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=Coronavirus+COVID+COVID19${userInputLocation}&sort=newest&api-key=9dBz5iLUOkToYiTEjcz0mgrNxq65pGzm`,
                 method: "GET"
             }).then(function (response) {
                 console.log(response.response.docs);  // r.r.docs accesses 10 first articles to match search criteria
-                
-            });
-            return response;
-        },
-        topArticles() {
-            apiNYTimes.call();
-            const articleDataVars = {
-                headline: response.response.docs[i].headline.main,
-            }
-            for (i = 0; i < 5; i++) {
-                console.log(articleDataVars.headline);
-            }
-        }
 
+                apiNYTimes.displayArticles(response.response.docs);
+            });
+        },
+        displayArticles(response) {
+            for (i = 0; i < 5; i++){
+                num = i + 1;
+                console.log(`Article ${num}: \nHEADLINE, ${response[i].headline.main} \nABSTRACT, ${response[i].abstract} \nWEB URL, ${response[i].web_url}`);
+                const headline = $("<div>").addClass(`article-headline card-content`).text(response[i].headline.main);
+                const abstract = $("<div>").addClass(`article-abstract card-content`).text(response[i].abstract);
+                const articleLink = $("<a>").addClass(`article-link card-content`).text('Read Full Article').attr('href', response[i].web_url);
+
+                const newsArticleDiv = $("<div>").addClass(`articles card-content`).attr("id", `news-article-${num}`).attr('value', i);
+                newsArticleDiv.append(headline, abstract, articleLink);
+                newsArticles.append(newsArticleDiv);
+            }
+        },
     }
 
-    // apiNYTimes.call();
-    apiNYTimes.topArticles();
+    apiNYTimes.call();
+    // apiNYTimes.topArticles();
 
 });
