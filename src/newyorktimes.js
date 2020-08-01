edb30332938128645501f6ba779c1ab5253aa4f7
BR3NDAN <br3ndan.l8n@gmail.com>
2020-08-01 13:43:50 -0400

    working on function to save articles. save button dynamically added

--- src/newyorktimes.js
+++ src/newyorktimes.js
@@ -22,18 +22,26 @@ $('document').ready(function () {
         displayArticles(response) {
             for (i = 0; i < 6; i++){
                 num = i + 1;
-                // console.log(`Article ${num}: \nHEADLINE, ${response[i].headline.main} \nABSTRACT, ${response[i].abstract} \nWEB URL, ${response[i].web_url}`);
                 const headline = $("<div>").addClass(`article-headline card-content`).text(response[i].headline.main);
                 const abstract = $("<div>").addClass(`article-abstract card-content`).text(response[i].abstract);
                 const articleLink = $("<a>").addClass(`article-link card-content`).text('Read Full Article').attr('href', response[i].web_url);
+                const saveButton = $("<button>").addClass('button').attr('type', 'button').attr('id', 'saveButton').text('Save');
 
-                const newsArticleDiv = $("<div>").addClass(`articles card-content col s12 m5`).attr("id", `news-article-${num}`).attr('value', `article-${i}`);
-                newsArticleDiv.append(headline, abstract, articleLink);
+                const newsArticleDiv = $("<div>").addClass(`articles card-content col s12 m5`).attr("data-name", article[i]);
+                newsArticleDiv.append(headline, abstract, articleLink, saveButton);
                 newsArticles.append(newsArticleDiv);
             }
         },
+        saveArticle() {
+            const article = $("<div>").attr('data-name');
+            console.log(article);
+        }
     }
 
     apiNYTimes.call();
+
+    $(document).on("click", "#saveButton", function () {
+        apiNYTimes.saveArticle();
+    });
 
 });
