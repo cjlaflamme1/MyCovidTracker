6b00b8aa45097e80cea5d2b547e480e0e1fa690e
BR3NDAN <br3ndan.l8n@gmail.com>
2020-08-01 12:55:07 -0400

    article card styling stuff

--- src/newyorktimes.js
+++ src/newyorktimes.js
@@ -20,14 +20,14 @@ $('document').ready(function () {
             });
         },
         displayArticles(response) {
-            for (i = 0; i < 5; i++){
+            for (i = 0; i < 6; i++){
                 num = i + 1;
-                console.log(`Article ${num}: \nHEADLINE, ${response[i].headline.main} \nABSTRACT, ${response[i].abstract} \nWEB URL, ${response[i].web_url}`);
+                // console.log(`Article ${num}: \nHEADLINE, ${response[i].headline.main} \nABSTRACT, ${response[i].abstract} \nWEB URL, ${response[i].web_url}`);
                 const headline = $("<div>").addClass(`article-headline card-content`).text(response[i].headline.main);
                 const abstract = $("<div>").addClass(`article-abstract card-content`).text(response[i].abstract);
                 const articleLink = $("<a>").addClass(`article-link card-content`).text('Read Full Article').attr('href', response[i].web_url);
 
-                const newsArticleDiv = $("<div>").addClass(`articles card-content`).attr("id", `news-article-${num}`).attr('value', i);
+                const newsArticleDiv = $("<div>").addClass(`articles card-content col s12 m5`).attr("id", `news-article-${num}`).attr('value', i);
                 newsArticleDiv.append(headline, abstract, articleLink);
                 newsArticles.append(newsArticleDiv);
             }
@@ -35,6 +35,5 @@ $('document').ready(function () {
     }
 
     apiNYTimes.call();
-    // apiNYTimes.topArticles();
 
 });
