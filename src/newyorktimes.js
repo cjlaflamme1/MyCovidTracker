4d05f2d2880d7ba002f97183b77fa452c6c200d7
BR3NDAN <br3ndan.l8n@gmail.com>
2020-08-02 15:57:43 -0400

    created array on load for saved articles, working on the loading and displaying function

--- src/newyorktimes.js
+++ src/newyorktimes.js
@@ -1,6 +1,7 @@
 $('document').ready(function () {
 
     const newsArticles = $('#newsArticles');
+    let savedNewsArticles = JSON.parse(localStorage.getItem("savedNewsArticles"));
 
     // NY Times API Object
     const apiNYTimes = {
@@ -15,11 +16,10 @@ $('document').ready(function () {
                 method: "GET"
             }).then(function (response) {
                 console.log(response.response.docs);  // r.r.docs accesses 10 first articles to match search criteria
-
-                apiNYTimes.displayArticles(response.response.docs);
+                apiNYTimes.displayRecentArticles(response.response.docs);
             });
         },
-        displayArticles(response) {
+        displayRecentArticles(response) {
             for (i = 0; i < 6; i++) {
                 num = i + 1;
                 const headline = $("<div>").addClass(`article-headline card-content`).text(response[i].headline.main);
@@ -35,7 +35,12 @@ $('document').ready(function () {
         saveArticle() {
             const article = $("<div>").attr('data-name');
             console.log(article);
-        }
+        },
+        loadSavedArticles() {
+            for (i = 0; i < savedNewsArticles.length; i++) {
+                console.log(savedNewsArticles[i]);
+            }
+        },
     }
 
     apiNYTimes.call();
@@ -43,5 +48,11 @@ $('document').ready(function () {
     $(document).on("click", "#saveButton", function () {
         apiNYTimes.saveArticle();
     });
+
+    if (Array.isArray(savedNewsArticles)) {  // does an array already exist in local storage?
+        apiNYTimes.loadSavedArticles();  // if so, load it
+    } else {
+        savedNewsArticles = [];  // else, create a new array to be saved
+    }
 
 });
