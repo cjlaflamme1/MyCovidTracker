2b4c21cd6df19250bb021ea48547399738d8b975
BR3NDAN <br3ndan.l8n@gmail.com>
2020-08-05 00:24:28 -0400

    NY Times api call now accepts multiple search terms

--- src/newyorktimes.js
+++ src/newyorktimes.js
@@ -1,15 +1,22 @@
 $('document').ready(function () {
+    // 
+    //  VARIABLES
+    // 
     const searchedCountryNameDisplay = $('#searchedCountryNameDisplay');
     const newsArticles = $('#newsArticles');
     const savedArticlesDisplay = $('#savedArticlesDisplay');
     let savedNewsArticles = JSON.parse(localStorage.getItem("savedNewsArticles"));
     let searchLocation = '';
-
+    // 
     // NY Times API Object
+    // 
     const apiNYTimes = {
         newestArticles: [],
-        refactorInputLocation() {
-            console.log('placeholder');
+        refactorSearchTerm(string) {
+            trimmedString = string.trim();
+            stringArray = trimmedString.split(' ');
+            newString = stringArray.join('+');
+            return newString;
         },
         call() {
             const userInputSearchTerm = $('#search');
@@ -23,7 +30,9 @@ $('document').ready(function () {
                 term = `Coronavirus+${userInputSearchTerm.val()}`;
                 console.log(`concatenated users term input`);
             }
-            let searchTerm = term;
+            let searchTerm = '';
+            searchTerm = apiNYTimes.refactorSearchTerm(term);
+            console.log("type of searchTerm =" + typeof(searchTerm));
             console.log(`search term length => ${searchTerm.length}`);
             console.log(`searchTerm => ${searchTerm}`);
             console.log(`userInputSearchTerm => ${userInputSearchTerm.val()}`);
@@ -38,7 +47,7 @@ $('document').ready(function () {
             });
         },
         callNew() {
-            
+
             $('.articles').remove();
             apiNYTimes.call();
         },
@@ -50,14 +59,14 @@ $('document').ready(function () {
 
                 const headline = $("<h3>").addClass(`article-title card-content`).text(headlineText).attr("data-name", `headline${i}`);
                 const articleLink = $("<a>").addClass(`article-link card-content`).text('Read Full Article').attr('href', articleLinkURL).attr('target', '_blank').attr("data-name", `link${i}`);
-                const saveButton = $("<button>").addClass('button waves-effect waves-light btn-small').attr('type', 'button').attr('id','saveButton').text('Save').attr("data-name", `${i}`);
+                const saveButton = $("<button>").addClass('button waves-effect waves-light btn-small').attr('type', 'button').attr('id', 'saveButton').text('Save').attr("data-name", `${i}`);
 
                 const newsArticleDiv = $("<div>").addClass(`articles card-content col s12 m4`).attr("data-name", `article${i}`);
                 newsArticleDiv.append(headline, articleLink, saveButton);
                 newsArticles.append(newsArticleDiv);
                 const recentArticleObject = {
-                        title: headlineText,
-                        link: articleLinkURL
+                    title: headlineText,
+                    link: articleLinkURL
                 }
                 apiNYTimes.newestArticles.push(recentArticleObject);
             }
@@ -73,15 +82,15 @@ $('document').ready(function () {
             savedArticlesDisplay.text('');
             for (i = 0; i < savedNewsArticles.length; i++) {
                 console.log(savedNewsArticles[i]);
-                
-                
+
+
                 const articleDiv = $('<div>').addClass('col card s12 m4 articles white-text card-content contentSections articleBlock').attr('data-name', `article-${i}`);
                 const titleHeader = $("<h2>").addClass(`article-title`).text(savedNewsArticles[i].title);
                 const articleLink = $("<a>").addClass(`article-link card-content`).text('Read Full Article').attr('href', savedNewsArticles[i].link).attr('target', '_blank');
-                const deleteButton = $("<button>").addClass('button waves-effect waves-light btn-small').attr('type', 'button').attr('id','deleteButton').text('Delete').attr("data-name", `${i}`);
+                const deleteButton = $("<button>").addClass('button waves-effect waves-light btn-small').attr('type', 'button').attr('id', 'deleteButton').text('Delete').attr("data-name", `${i}`);
                 articleDiv.append(titleHeader, articleLink, deleteButton);
                 savedArticlesDisplay.append(articleDiv);
-                
+
             }
         },
         deleteArticle() {
@@ -93,19 +102,21 @@ $('document').ready(function () {
         }
     }
 
+    // 
+    //  EVENT LISTENERS - click click
+    //     
     $(document).on("click", "#saveButton", apiNYTimes.saveArticle);
     $(document).on("click", "#deleteButton", apiNYTimes.deleteArticle);
     $(document).on('click', '.newContentBtn', apiNYTimes.callNew);
-    
-
-    if(window.location.pathname.includes("data.html")){
+    // 
+    //  RUN ON PAGE LOAD
+    //     
+    if (window.location.pathname.includes("data.html")) {
         $(window).on('load', apiNYTimes.call);
     }
-
     if (Array.isArray(savedNewsArticles)) {  // does an array already exist in local storage?
         apiNYTimes.loadSavedArticles();  // if so, load it
     } else {
         savedNewsArticles = [];  // else, create a new array to be saved
     }
-    console.log(window.location.search);
 });
